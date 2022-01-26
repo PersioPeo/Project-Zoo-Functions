fab5389b3743d0fb6dc9c2299808e0696026e4be
Pérsio Pereira <persiopeo@gmail.com>
2022-01-26 11:22:34 -0300

    Adicionando o Item - 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalSelecionado = data.species.filter((eleAtual) => eleAtual.name === animal);
+  const animalIdadeMinima = animalSelecionado[0].residents.every((eleAtual) => eleAtual.age >= 7);
+  return animalIdadeMinima;
 }
 
 module.exports = getAnimalsOlderThan;
