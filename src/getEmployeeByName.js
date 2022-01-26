ecd66ad83db1c0f3ea8ebf822fe050383d2ef903
Pérsio Pereira <persiopeo@gmail.com>
2022-01-26 12:19:56 -0300

    Adicionando o Item - 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const primeiroNome = data.employees.find((elemAtual) => elemAtual.firstName === employeeName);
+  const ultimoNome = data.employees.find((elemAtual) => elemAtual.lastName === employeeName);
+  if (primeiroNome === undefined) {
+    return ultimoNome;
+  } if (ultimoNome === undefined) {
+    return primeiroNome;
+  }
 }
 
 module.exports = getEmployeeByName;
