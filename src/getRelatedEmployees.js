cee9b4999409f9fab13cd43ee8781ac25cbae1d0
Pérsio Pereira <persiopeo@gmail.com>
2022-01-26 22:05:45 -0300

    Adicionando o Item - 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const objfuncionario = data.employees;
+
 function isManager(id) {
   // seu código aqui
+  const gerencia = [];
+  for (let i = 0; i < objfuncionario.length; i += 1) {
+    const elemAtual = objfuncionario[i];
+    gerencia.push(...elemAtual.managers);
+  }
+  return gerencia.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const colaboradoresDaGenrecia = objfuncionario.filter(
+      (elemAtual) => elemAtual.managers.includes(managerId),
+    );
+    const funcionariosNomeCompleto = colaboradoresDaGenrecia.map(
+      (elemAtual) => `${elemAtual.firstName} ${elemAtual.lastName}`,
+    );
+    return funcionariosNomeCompleto;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
