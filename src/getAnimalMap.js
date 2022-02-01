eac80620d1b40926779dd8cad3cc7c9bcd4f1655
Pérsio Pereira <persiopeo@gmail.com>
2022-02-01 12:21:08 -0300

    Adicionando Item - 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
+const geraListaDeNomesComSexo = (element, sex) => {
+  if (sex === undefined) {
+    return element.name;
+  }
+  if (element.sex === sex) {
+    return element.name;
+  }
+};
+const organiza = (nomes, sort) => {
+  if (sort === true) {
+    return nomes.sort();
+  }
+  return nomes;
+};
+const listaDeNomes = () => {
+  const animalPorArea = { NE: [], NW: [], SE: [], SW: [] };
+  data.species.forEach((elemento) => {
+    animalPorArea[elemento.location].push(elemento.name);
+  });
+  return animalPorArea;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const animalPorArea = { NE: [], NW: [], SE: [], SW: [] };
+  if (!options || !options.includeNames) {
+    return listaDeNomes();
+  }
+  data.species.forEach((elemento) => {
+    let arrayDeNomes = [];
+    elemento.residents.forEach((element) => {
+      const nomeParaVerificar = geraListaDeNomesComSexo(element, options.sex);
+      if (nomeParaVerificar !== undefined) {
+        arrayDeNomes.push(geraListaDeNomesComSexo(element, options.sex));
+      }
+    });
+    arrayDeNomes = organiza(arrayDeNomes, options.sorted);
+    const tipoENome = { [elemento.name]: arrayDeNomes };
+    animalPorArea[elemento.location].push(tipoENome);
+  });
+  return animalPorArea;
 }
 
 module.exports = getAnimalMap;
