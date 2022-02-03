9ec4f4ac685bd98d5021e674d10e7a69af41b54f
Pérsio Pereira <persiopeo@gmail.com>
2022-02-02 23:05:34 -0300

    Adicionado o item - 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const animal = data.species;
+const funcionario = data.employees;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const selectPrimeiorAnimal = (idRecebido) => {
+    const objFuncionario = funcionario.find(
+      (element) => element.id === idRecebido,
+    ).responsibleFor[0];
+    return objFuncionario;
+  };
+  const idDoAnimal = selectPrimeiorAnimal(id);
+  const verificaAnimal = (idAnimal) => {
+    const nomeAnimal = animal.find((element2) => element2.id === idAnimal);
+    const residents = nomeAnimal.residents.reduce(
+      (element3, current) => ((element3.age > current.age) ? element3 : current),
+    );
+    const array = [residents.name, residents.sex, residents.age];
+    return array;
+  };
+  return verificaAnimal(idDoAnimal);
 }
-
+// console.log(getOldestFromFirstSpecies(id));
 module.exports = getOldestFromFirstSpecies;
